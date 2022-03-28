ad455773e52f77a127f6e5a0717e3e6a7dd30079
EpicNewt <epicnewt@gmail.com>
2022-03-28 19:38:02 +0100

    Huge performance boost path generator
    Generating the step in each interation, which happened billions
    of times has a massive performance overhead

--- lib/mass_outbreak/search/rng/path/mo/pass_path_generator.dart
+++ lib/mass_outbreak/search/rng/path/mo/pass_path_generator.dart
@@ -1,15 +1,21 @@
 
-Iterable<List<int>> passivePaths(int spawns, {int depth = 1, int maxDepth = 20}) sync* {
+Iterable<List<int>> passivePaths(int spawns, {int depth = 1, int maxDepth = 20, baseLimit = 11}) sync* {
   for (int d = depth; d <= maxDepth; d++) {
     var maxDespawns = BigInt.from(spawns - 4);
-    final base = maxDespawns + BigInt.one;
+    final base = (maxDespawns >= BigInt.from(baseLimit)) ? BigInt.from(baseLimit) : (maxDespawns + BigInt.one);
+    print("depth: $d, base: $base");
     var start = BigInt.zero;
     var end = (base - BigInt.one) * base.pow(d - 1);
     var step = BigInt.one;
     List<int> path = List.filled(d, 0);
+    List<BigInt> bases = List.generate(d, (index) => base.pow(index));
 
     for (var p = start; p <= end; p += step) {
       var pathSum = BigInt.zero;
+
+      if (depth != maxDepth && (p % base) > BigInt.zero) {
+        continue;
+      }
 
       var r = p;
       var i = path.length - 1;
@@ -26,10 +32,11 @@ Iterable<List<int>> passivePaths(int spawns, {int depth = 1, int maxDepth = 20})
 
       if (pathSum == maxDespawns) {
         for (i = 0; i < d; i++) {
-          if (path[path.length - 1 - i] != 0) {
+          var index = path.length - 1 - i;
+          if (path[index] != 0) {
             break;
           }
-          step = base.pow(i);
+          step = bases[index];
         }
       } else if (pathSum < maxDespawns) {
         step = BigInt.one;
@@ -38,7 +45,14 @@ Iterable<List<int>> passivePaths(int spawns, {int depth = 1, int maxDepth = 20})
       }
 
       yield path;
-      path = List.filled(d, 0);
+      // path = List.filled(d, 0);
     }
   }
+}
+
+main() {
+  var sw = Stopwatch();
+  for (var value in passivePaths(15)) {
+  }
+  print(sw.elapsed);
 }
