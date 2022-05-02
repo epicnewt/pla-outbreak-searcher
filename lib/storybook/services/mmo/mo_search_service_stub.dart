993abe0e55926a802ed5d8927d58169127e3dead
EpicNewt <epicnewt@gmail.com>
2022-05-02 18:20:57 +0100

    MO search service stub for storybooks

---
+++ lib/storybook/services/mmo/mo_search_service_stub.dart
@@ -0,0 +1,25 @@
+
+import 'package:get_it/get_it.dart';
+import 'package:mmo_searcher/mass_outbreak/search/model/mass_outbreak_search_data.dart';
+import 'package:mmo_searcher/mass_outbreak/search/model/mass_outbreak_information.dart';
+import 'package:mmo_searcher/mass_outbreak/search/model/mass_outbreak_searcher.dart';
+import 'package:mmo_searcher/mass_outbreak/search/rng/path/mo/search.dart';
+import 'package:mmo_searcher/mass_outbreak/search/model/search_result.dart';
+import 'package:mmo_searcher/pokedex/pokedex.dart';
+
+class MOSearchServiceStub implements MassOutbreakSearcher {
+  @override
+  Future<MassOutbreakInformation> gatherOutbreakInformation() async {
+    return MassOutbreakInformation("DA4676C773234F", 10, 25);
+  }
+
+  @override
+  Future<SearchResult> performSearch(MassOutbreakInformation moInfo, MassOutbreakSearchData filters, int rolls, bool multimatch, void Function(MassOutbreakResult? p1) onMatch, Function onTick) async {
+    return SearchResult(75643, [1,2,4,4,0,0,0,0,6], -1, pokedex[25]!);
+  }
+
+
+  static void register() {
+    GetIt.I.registerSingleton<MassOutbreakSearcher>(MOSearchServiceStub());
+  }  
+}
